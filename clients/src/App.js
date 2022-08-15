353564036be497a5b82df784d40d71c166b84e38
GodsonAddy <70777404+GodsonAddy@users.noreply.github.com>
2022-08-15 14:45:40 +0000

    HashRouter

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import "./App.css";
+// Use HashRouter when you deploy to Heroku/Netlify
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Navigate,
   Route,
   Routes,
